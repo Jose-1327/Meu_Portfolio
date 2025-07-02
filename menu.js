714e1c1c15b8cb7551745165ac975f4fbbcba94b
José <joseasmorais13@gmail.com>
2025-07-02 17:53:37 -0300

    Certificacoes

--- menu.js
+++ menu.js
@@ -3,16 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     accordionButtons.forEach(button => {
         button.addEventListener('click', () => {
-            // Encontra o item de sanfona pai
+
             const accordionItem = button.closest('.accordion-item');
-            // Encontra o conteúdo do item de sanfona
+
             const accordionContent = accordionItem.querySelector('.accordion-content');
 
-            // Alterna a classe 'active' no botão e no conteúdo
             button.classList.toggle('active');
             accordionContent.classList.toggle('active');
 
-            // Ajusta a altura máxima para a transição
             if (accordionContent.classList.contains('active')) {
                 accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
             } else {
